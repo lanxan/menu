4c1ca08327d08f42667b33bc002eba4b2fdf4106
李翌辰 <lanxan0309@gmail.com>
2015-04-20 18:03:40 +0800

    prototype finished

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,44 @@
 (function ($) {
     
-    function menu( options ){
+    $.fn.menu = function( options ){
 		var container = this;
-		var settings = $.extend(true, {
+
+		var defaults = {
             topBar : {
                 Home : {
-                    a : 'index.html',
+                    a : 'index.html'
                 },
                 Document : {
-                    a : 'doc.html',
+                    a : 'doc.html'
                 },
                 About : {
-                    a : 'about.html',
+                    a : 'about.html'
                 }
             }
-        }, options);
-	
-		var topbarProto = {
+		}
+		var settings = $.extend({}, defaults, options);
+
+		console.log(settings);	
+		var topBarProto = {
 			attr : {
-				class : 'topBar',
+				class : 'topBar'
 			},
 			css : {
-				height : '30px',
+				height : '20px',
+				width  : '100px',
+				padding: '5px 3px 0px',
+				margin : '1px',
+				border : '2px solid',
+				textAlign: 'left'
 			}
 		}
 		
 		$.each(settings.topBar, function(i, e){
-			$('<li>' + i + '</li>')
-			.attr( topbarProto.attr )
-			.css( topbarProto.css )
-			.appendTo('body');
-			//.append('<a href='+e.a+'></a>')
+			$('<ul />')
+			.attr( topBarProto.attr )
+			.css( topBarProto.css )
+			.append('<a href='+e.a+'>' + i +'</a>')
+			.appendTo(container);
 		});
 	}
 }( jQuery ));
